4f741c33ef536f1551c3c291fecb4c45ff27a19d
recardona <me@rogel.io>
2013-02-16 22:31:41 -0500

    Added a basic action class, which acts just like a State, but without conditions.

---
+++ src/week1/action.py
@@ -0,0 +1,37 @@
+'''
+Created on Feb 16, 2013
+
+@author: recardon
+'''
+
+class Action():
+    """
+    Represents things an agent can do to change the state of the world.  In
+    this representation, a name is the only thing that defines an action.
+    """
+    
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return "Action {}".format(self.name);
+    
+    def __eq__(self, other):
+        """
+        Action equality relies on type and name equality.
+        """
+        if(not isinstance(other, Action)):
+            return False
+        
+        if(self.name != other.name):
+            return False
+        
+        return True
+        
+    def __ne__(self, other):
+        """
+        Action inequality must have inequality in the type or name
+        """
+        return not self.__eq__(other)
+        
+        
