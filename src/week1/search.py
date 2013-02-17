d7e3e8758736c7fb1e0036f39012f68bf0e0875f
recardona <me@rogel.io>
2013-02-16 23:10:47 -0500

    Defining skeletons for the essential components of a Search process:  A SearchNode and a SearchProblem

---
+++ src/week1/search.py
@@ -0,0 +1,29 @@
+'''
+Created on Feb 16, 2013
+
+@author: recardon
+'''
+
+class SearchNode():
+    """
+    A Search Node is a data structure that we manipulate during the search
+    process.  Search nodes are nodes in the search tree.  It is characterized
+    by the state it represents, a parent node, the action that gets us from
+    the parent node's state to this state, the path cost of reaching this node
+    and the depth of this node in the search tree.
+    """
+    
+    def __init__(self):
+        pass
+
+class SearchProblem():
+    """
+    A Search Problem is characterized by an initial state, a set of possible
+    actions and applicability conditions, a goal state, and a path cost
+    function.
+    """
+    
+    def __init__(self):
+        pass
+    
+    
