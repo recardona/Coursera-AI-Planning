6268d4e65b064faf6740d87eb529c33abe031e42
Rogelio E. Cardona-Rivera <me@rogel.io>
2013-02-18 14:44:52 -0500

    Implemented the SearchNode and SearchProblem constructors, as well as an auxiliary method to determine whether or not a SearchNode is the root of the search tree.

--- src/week1/search.py
+++ src/week1/search.py
@@ -13,8 +13,31 @@ class SearchNode():
     and the depth of this node in the search tree.
     """
     
-    def __init__(self):
-        pass
+    def __init__(self, state, parent_node=None, action=None, path_cost=0, depth=0):
+        """
+        Initializes a SearchNode with the given parameters.  Default parameters
+        are for when the node is the root node of the search tree.
+        """
+        self.state = state
+        self.parent_node = parent_node 
+        self.action = action
+        self.path_cost = path_cost
+        self.depth = depth 
+
+    def __str__(self):
+        if self.isRoot():
+            return "Root SearchNode for State: {0}".format(self.state)
+        
+        else:
+            return "SearchNode for State: {0}, with parent {1} through Action \
+            {2}\n\tPath Cost: {3}\t\tDepth:{4}".format(self.state, \
+                                                       self.parent_node.state, \
+                                                       self.path_cost, self.depth)
+    
+    def isRoot(self):
+        return (self.parent_node == None and self.action == None and \
+                self.path_cost == 0 and self.depth == 0)
+
 
 class SearchProblem():
     """
@@ -23,7 +46,13 @@ class SearchProblem():
     function.
     """
     
-    def __init__(self, init_state, successor_function, goal_state, path_cost_function):
+    def __init__(self, init_state, successor_function, goal_state, \
+                 path_cost_function=lambda state: state.depth*1):
+        """
+        The Search Problem is defined by an initial state, a successor function,
+        and a goal state.  In lieu of a path cost function, a default one is
+        provided that depends on the depth of the node in the tree.
+        """
         self.initState = init_state
         self.successorFunction = successor_function
         self.goalState = goal_state
